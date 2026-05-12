923afc38e0a97aabfaddd28ba18ceddd422a26bd
Maxi <Chiesakarkovichm@gmail.com>
2026-05-11 21:48:56 -0300

    Refactor Sumar function to use reduce for summing multiple numbers from a comma-separated string

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ export function Sumar(numeros) {
   }
   if (numeros.includes(',')) {
     const partes = numeros.split(',');
-    return parseInt(partes[0], 10) + parseInt(partes[1], 10) + 
-    (partes[2] ? parseInt(partes[2], 10) : 0);
+    return partes.reduce((suma, parte) => suma + parseInt(parte, 10), 0);
   }
 
   return parseInt(numeros, 10);
