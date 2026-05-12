e61840f7abcc67117a7c78a92faa81a49eb9f056
Maxi <Chiesakarkovichm@gmail.com>
2026-05-11 21:47:36 -0300

    Enhance Sumar function to support summing three numbers from a comma-separated string

--- index.js
+++ index.js
@@ -4,7 +4,8 @@ export function Sumar(numeros) {
   }
   if (numeros.includes(',')) {
     const partes = numeros.split(',');
-    return parseInt(partes[0], 10) + parseInt(partes[1], 10);
+    return parseInt(partes[0], 10) + parseInt(partes[1], 10) + 
+    (partes[2] ? parseInt(partes[2], 10) : 0);
   }
 
   return parseInt(numeros, 10);
